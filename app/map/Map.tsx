e7e50b680115f69594595727623324323f216688
andreialex6 <alex.girleanu@yahoo.com>
2025-12-03 19:25:18 +0200

    update

--- app/map/Map.tsx
+++ app/map/Map.tsx
@@ -11,7 +11,7 @@ export default function ArcgisMap() {
   useEffect(() => {
     if (!mapRef.current) return;
 
-    Config.apiKey = "AAPTxy8BH1VEsoebNVZXo8HurPIJY1v-j0MUAqs2Z7fFgLH-LLzdltEm8uGCX0FpcdbN_tDJymY37qNmEKMYZo2iM-03V-QThvjbyd4L6BCyIvuqAeQlvgCHd6pKb7bWKq_1uJpOA2RBe1UUbjPXd3BQAFUB68B3EqdtmwvN7WFkFYh5dupp8EDV1goLKxg-ET80r7AHsC91cyMOAoZv7-j5ozbzmO1uL8_dtyHfti0h_xM.AT1_xj5BfVfv";
+    Config.apiKey = process.env.NEXT_PUBLIC_ARCGIS_API_KEY;
 
     const map = new Map({
       basemap: "streets-vector"
